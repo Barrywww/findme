9755910926933c00e1c407267092a29c576ef46f
Barry Wang <36621849+Barrywww@users.noreply.github.com>
2023-01-27 00:25:08 -0500

    init: project setup

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      files: ['webpack.config.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 0,
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    semi: 0,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-filename-extension': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+}
---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const PostcssFlexbugsFixes = require('postcss-flexbugs-fixes')
+const PostcssPresetEnv = require('postcss-preset-env')
+const PostcssNormalize = require('postcss-normalize')
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+const getStyleLoaders = (importLoaders) => [
+  'style-loader',
+  {
+    loader: 'css-loader',
+    options: {
+      sourceMap: isDev,
+      importLoaders,
+    },
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      ident: 'postcss',
+      plugins: [
+        PostcssFlexbugsFixes(),
+        PostcssPresetEnv({
+          autoprefixer: {
+            grid: true,
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+        }),
+        PostcssNormalize(),
+      ],
+      sourceMap: isDev,
+    },
+  },
+]
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    filename: `[name]${isDev ? '' : '.[hash:8]'}.js`,
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    hot: true,
+    compress: true,
+  },
+  devtool: isDev ? 'eval-source-map' : '',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+      minify: !isDev,
+      cache: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?|js)$/,
+        loader: 'babel-loader',
+        options: { cacheDirectory: true },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: getStyleLoaders(1),
+      },
+      {
+        test: /\.less$/,
+        use: [
+          ...getStyleLoaders(2),
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          ...getStyleLoaders(2),
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(svg|png|gif|jpe?g|woff2?|ttf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}
